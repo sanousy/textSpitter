9e60fecdbeeb4cd90573684d88d4955dab3f8735
Sanousy <sanousy73@gmail.com>
2020-07-19 23:39:27 +0300

    Add files via upload

---
+++ Spliter/src/spliter/Spliter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*/
+
+package spliter;
+
+/**
+ *
+ * @author Invenio
+ */
+public class Spliter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JSplitter sp=  new JSplitter();
+        sp.setVisible(true);
+    }
+    
+}
